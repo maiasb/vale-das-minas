6e4657450bff7f0f9a183d996094f954af0500ed
Caio Maia <caio.maiasb@gmail.com>
2025-10-28 16:35:51 -0300

    configuração pra deploy com domínio

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vale-das-minas"
+  // APENAS BARRA QUANDO FOR DOMÍNIO PERSONALIZADO
+  base: "/"
+  // base: "/vale-das-minas"
 })
